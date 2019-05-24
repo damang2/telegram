53014976fd40723dccdc504d5cc20b306e155679
damang2 <50958605+damang2@users.noreply.github.com>
2019-05-24 12:44:03 +0800

    Update packages.sh

--- bch/packages.sh
+++ bch/packages.sh
@@ -9,15 +9,11 @@ apt upgrade -y
 pkg install python -y
 pip install --upgrade pip
 pip install -r requirements.txt
-rm -rf telethon
-clear
-unzip telethon.zip 
 
 echo
 echo -e "\e[1n Subscribe to Termux Tricks & Tut"
 echo
-echo -e "\e[1m\e[32m Enter :\e[33m python main.py (country_code_area_code_number)"
+echo -e "\e[1m\e[32m Enter :\e[33m python bch.py (country_code_area_code_number)"
 echo
-echo -e "\e[1m\e[32m Example :\e[33m python main.py +639359094586"
-echo
+echo -e "\e[1m\e[32m Example :\e[33m python bch.py +639359094586"
 echo

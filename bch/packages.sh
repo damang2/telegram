8f9e82e2d0e0dec36715b8a862c80aef7c46d643
damang2 <50958605+damang2@users.noreply.github.com>
2019-05-26 17:35:05 +0800

    Update packages.sh

--- bch/packages.sh
+++ bch/packages.sh
@@ -13,7 +13,5 @@ pip install -r requirements.txt
 echo
 echo -e "\e[1n Subscribe to Termux Tricks & Tut"
 echo
-echo -e "\e[1m\e[32m Enter :\e[33m python bch.py (country_code_area_code_number)"
-echo
-echo -e "\e[1m\e[32m Example :\e[33m python bch.py +639359094586"
+echo -e "\e[1m\e[32m Enter :\e[33m exit"
 echo
